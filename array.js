beb36db294676a94408c5e98c2f926ea04144eda
krishnasorathiya167 <142407148+krishnasorathiya167@users.noreply.github.com>
2024-05-07 18:59:18 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,44 @@
+// add 
+// let arr = [2,5,64,43,54,33,12]
+
+// let index = 3;
+// let element = 78;
+
+// for(let i=arr.length-1;i>=0;i--){
+//     if (i >= index) {
+//         arr[i + 1]= arr[i];
+//         if (i === index) {
+//             arr[i]= element;
+//         }
+//     }
+// }
+// console.log(arr);
+
+
+// prime number
+
+// for (let i = 1; i <= 100; i++) {
+//     let found = false;
+//     for (j = 2; j <= i; j++) {
+//         if (i % j == 0 && j != i) {
+//             found = true;
+//         }
+//     }
+//     if (!found) {
+//         console.log(i);
+//     }
+// }
+
+// update
+
+// let arr = [12,3,41,17,62];
+
+// let element = 2;
+// let position = 2;
+
+// for(i=0;i<arr.length;i++){
+//     if(i === position-1){
+//         arr[i]=position;
+//     }
+// }
+// console.log(arr);
